871c015c6e4143aae1e5aaf0e5fd4a528cfeba21
Neil Lyons <nwjlyons@googlemail.com>
2022-04-17 14:20:50 +0100

    Use assertEqual helper func in tests

--- slice_test.go
+++ slice_test.go
@@ -18,21 +18,21 @@ func TestAll(t *testing.T) {
 	isEven := func(number int) bool {
 		return number%2 == 0
 	}
-	assert(t, slice.All([]int{1, 2, 4, 6, 8}, isEven), false)
-	assert(t, slice.All([]int{2, 4, 6, 8}, isEven), true)
+	assertEqual(t, slice.All([]int{1, 2, 4, 6, 8}, isEven), false)
+	assertEqual(t, slice.All([]int{2, 4, 6, 8}, isEven), true)
 }
 
 func TestAny(t *testing.T) {
 	isEven := func(number int) bool {
 		return number%2 == 0
 	}
-	assert(t, slice.Any([]int{1, 3, 5, 7, 9}, isEven), false)
-	assert(t, slice.Any([]int{1, 3, 2, 7, 9}, isEven), true)
+	assertEqual(t, slice.Any([]int{1, 3, 5, 7, 9}, isEven), false)
+	assertEqual(t, slice.Any([]int{1, 3, 2, 7, 9}, isEven), true)
 }
 
 func TestCount(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	assert(t, slice.Count(numbers), 9)
+	assertEqual(t, slice.Count(numbers), 9)
 }
 
 func TestCountBy(t *testing.T) {
@@ -40,7 +40,7 @@ func TestCountBy(t *testing.T) {
 	isEven := func(number int) bool {
 		return number%2 == 0
 	}
-	assert(t, slice.CountBy(numbers, isEven), 4)
+	assertEqual(t, slice.CountBy(numbers, isEven), 4)
 }
 
 func TestEach(t *testing.T) {
@@ -53,13 +53,13 @@ func TestFilter(t *testing.T) {
 	got := slice.Filter(numbers, func(number int) bool {
 		return number%2 == 0
 	})
-	assert(t, reflect.DeepEqual(got, []int{2, 4, 6, 8}), true)
+	assertEqual(t, got, []int{2, 4, 6, 8})
 }
 
 func TestFrequencies(t *testing.T) {
 	frequencies := slice.Frequencies([]string{"aa", "aa", "bb", "cc"})
 	expected := map[string]int{"aa": 2, "bb": 1, "cc": 1}
-	assert(t, reflect.DeepEqual(frequencies, expected), true)
+	assertEqual(t, frequencies, expected)
 }
 
 func TestFrequenciesBy(t *testing.T) {
@@ -67,7 +67,7 @@ func TestFrequenciesBy(t *testing.T) {
 		return strings.ToLower(element)
 	})
 	expected := map[string]int{"aa": 2, "bb": 1, "cc": 1}
-	assert(t, reflect.DeepEqual(frequencies, expected), true)
+	assertEqual(t, frequencies, expected)
 }
 
 func TestGroupBy(t *testing.T) {
@@ -76,13 +76,13 @@ func TestGroupBy(t *testing.T) {
 	got := slice.GroupBy(planets, func(planet string) int {
 		return len(planet)
 	})
-	assert(t, reflect.DeepEqual(got, expected), true)
+	assertEqual(t, got, expected)
 }
 
 func TestIsMember(t *testing.T) {
 	planets := []string{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"}
-	assert(t, slice.IsMember(planets, "Earth"), true)
-	assert(t, slice.IsMember(planets, "Pluto"), false)
+	assertEqual(t, slice.IsMember(planets, "Earth"), true)
+	assertEqual(t, slice.IsMember(planets, "Pluto"), false)
 }
 
 func TestMap(t *testing.T) {
@@ -91,18 +91,18 @@ func TestMap(t *testing.T) {
 		return light + "!"
 	})
 	expected := []string{"red!", "amber!", "green!"}
-	assert(t, reflect.DeepEqual(got, expected), true)
+	assertEqual(t, got, expected)
 
 	numbers := []int{1, 2, 3}
 	numbersAsStrings := slice.Map(numbers, func(number int) string {
 		return strconv.Itoa(number)
 	})
-	assert(t, reflect.DeepEqual(numbersAsStrings, []string{"1", "2", "3"}), true)
+	assertEqual(t, numbersAsStrings, []string{"1", "2", "3"})
 }
 
 func TestMax(t *testing.T) {
 	numbers := []int{6, 4, 8, 2, 1, 9, 4, 7, 5}
-	assert(t, slice.Max(numbers), 9)
+	assertEqual(t, slice.Max(numbers), 9)
 }
 
 func TestMaxBy(t *testing.T) {
@@ -114,12 +114,12 @@ func TestMaxBy(t *testing.T) {
 	maxPlanet := slice.MaxBy(planets, func(planet planet) int {
 		return planet.Radius
 	})
-	assert(t, maxPlanet.Name, jupiter.Name)
+	assertEqual(t, maxPlanet.Name, jupiter.Name)
 }
 
 func TestMin(t *testing.T) {
 	numbers := []int{6, 4, 8, 2, 1, 9, 4, 7, 5}
-	assert(t, slice.Min(numbers), 1)
+	assertEqual(t, slice.Min(numbers), 1)
 }
 
 func TestMinBy(t *testing.T) {
@@ -131,14 +131,14 @@ func TestMinBy(t *testing.T) {
 	minPlanet := slice.MinBy(planets, func(planet planet) int {
 		return planet.Radius
 	})
-	assert(t, minPlanet.Name, mars.Name)
+	assertEqual(t, minPlanet.Name, mars.Name)
 }
 
 func TestMinMax(t *testing.T) {
 	numbers := []int{6, 4, 8, 2, 1, 9, 4, 7, 5}
 	min, max := slice.MinMax(numbers)
-	assert(t, min, 1)
-	assert(t, max, 9)
+	assertEqual(t, min, 1)
+	assertEqual(t, max, 9)
 }
 
 func TestMinMaxBy(t *testing.T) {
@@ -150,8 +150,8 @@ func TestMinMaxBy(t *testing.T) {
 	minPlanet, maxPlanet := slice.MinMaxBy(planets, func(planet planet) int {
 		return planet.Radius
 	})
-	assert(t, minPlanet.Name, mars.Name)
-	assert(t, maxPlanet.Name, jupiter.Name)
+	assertEqual(t, minPlanet.Name, mars.Name)
+	assertEqual(t, maxPlanet.Name, jupiter.Name)
 }
 
 func TestReduce(t *testing.T) {
@@ -160,7 +160,7 @@ func TestReduce(t *testing.T) {
 		return acc + planet
 	}, "")
 	expected := "MercuryVenusEarthMarsJupiterSaturnUranusNeptune"
-	assert(t, got, expected)
+	assertEqual(t, got, expected)
 }
 
 func TestReduceWhile(t *testing.T) {
@@ -171,7 +171,7 @@ func TestReduceWhile(t *testing.T) {
 		}
 		return slice.Cont, number + total
 	}, 0)
-	assert(t, got, 42)
+	assertEqual(t, got, 42)
 }
 
 func TestReject(t *testing.T) {
@@ -179,19 +179,19 @@ func TestReject(t *testing.T) {
 	got := slice.Reject(numbers, func(number int) bool {
 		return number%2 == 0
 	})
-	assert(t, reflect.DeepEqual(got, []int{1, 3, 5, 7, 9}), true)
+	assertEqual(t, got, []int{1, 3, 5, 7, 9})
 }
 
 func TestReverse(t *testing.T) {
 	planets := []string{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"}
 	expected := []string{"Neptune", "Uranus", "Saturn", "Jupiter", "Mars", "Earth", "Venus", "Mercury"}
-	assert(t, reflect.DeepEqual(slice.Reverse(planets), expected), true)
+	assertEqual(t, slice.Reverse(planets), expected)
 }
 
 func TestSort(t *testing.T) {
 	numbers := []int{5, 6, 1, 3, 7, 8, 2, 4, 9}
-	assert(t, reflect.DeepEqual(slice.Sort(numbers, slice.Asc), []int{1, 2, 3, 4, 5, 6, 7, 8, 9}), true)
-	assert(t, reflect.DeepEqual(slice.Sort(numbers, slice.Desc), []int{9, 8, 7, 6, 5, 4, 3, 2, 1}), true)
+	assertEqual(t, slice.Sort(numbers, slice.Asc), []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	assertEqual(t, slice.Sort(numbers, slice.Desc), []int{9, 8, 7, 6, 5, 4, 3, 2, 1})
 }
 
 func TestSortBy(t *testing.T) {
@@ -201,13 +201,13 @@ func TestSortBy(t *testing.T) {
 
 	planets := []planet{neptune, mars, jupiter}
 
-	assert(t, reflect.DeepEqual(slice.SortBy(planets, func(planet planet) int {
+	assertEqual(t, slice.SortBy(planets, func(planet planet) int {
 		return planet.Radius
-	}, slice.Asc), []planet{mars, neptune, jupiter}), true)
+	}, slice.Asc), []planet{mars, neptune, jupiter})
 
-	assert(t, reflect.DeepEqual(slice.SortBy(planets, func(planet planet) int {
+	assertEqual(t, slice.SortBy(planets, func(planet planet) int {
 		return planet.Radius
-	}, slice.Desc), []planet{jupiter, neptune, mars}), true)
+	}, slice.Desc), []planet{jupiter, neptune, mars})
 }
 
 func TestSplitWhile(t *testing.T) {
@@ -215,8 +215,8 @@ func TestSplitWhile(t *testing.T) {
 	left, right := slice.SplitWhile(numbers, func(number int) bool {
 		return number <= 5
 	})
-	assert(t, reflect.DeepEqual(left, []int{1, 2, 3, 4, 5}), true)
-	assert(t, reflect.DeepEqual(right, []int{6, 7, 8, 9}), true)
+	assertEqual(t, left, []int{1, 2, 3, 4, 5})
+	assertEqual(t, right, []int{6, 7, 8, 9})
 }
 
 func TestSplitWith(t *testing.T) {
@@ -224,13 +224,13 @@ func TestSplitWith(t *testing.T) {
 	evenNumbers, oddNumbers := slice.SplitWith(numbers, func(number int) bool {
 		return number%2 == 0
 	})
-	assert(t, reflect.DeepEqual(evenNumbers, []int{2, 4, 6, 8}), true)
-	assert(t, reflect.DeepEqual(oddNumbers, []int{1, 3, 5, 7, 9}), true)
+	assertEqual(t, evenNumbers, []int{2, 4, 6, 8})
+	assertEqual(t, oddNumbers, []int{1, 3, 5, 7, 9})
 }
 
 func TestSum(t *testing.T) {
 	numbers := []int{6, 4, 8, 2, 1, 9, 4, 7, 5}
-	assert(t, slice.Sum(numbers), 46)
+	assertEqual(t, slice.Sum(numbers), 46)
 }
 
 func TestSumBy(t *testing.T) {
@@ -242,18 +242,24 @@ func TestSumBy(t *testing.T) {
 	sum := slice.SumBy(planets, func(planet planet) int {
 		return planet.Radius
 	})
-	assert(t, sum, mars.Radius+jupiter.Radius+neptune.Radius)
+	assertEqual(t, sum, mars.Radius+jupiter.Radius+neptune.Radius)
 }
 
 func TestTake(t *testing.T) {
 	planets := []string{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"}
-	assert(t, reflect.DeepEqual(slice.Take(planets, 2), []string{"Mercury", "Venus"}), true)
-	assert(t, reflect.DeepEqual(slice.Take(planets, 10), planets), true)
-	assert(t, reflect.DeepEqual(slice.Take(planets, 0), []string{}), true)
+	assertEqual(t, slice.Take(planets, 2), []string{"Mercury", "Venus"})
+	assertEqual(t, slice.Take(planets, 10), planets)
+	assertEqual(t, slice.Take(planets, 0), []string{})
 }
 
-func assert[T comparable](t *testing.T, got T, expected T) {
-	if got != expected {
-		t.Errorf("got:\n%v\nexpected:\n%v\n", got, expected)
+func assertEqual[T any](t *testing.T, got T, expected T) {
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("\n     got: %v\nexpected: %v\n", got, expected)
+	}
+}
+
+func assertNotEqual[T any](t *testing.T, got T, expected T) {
+	if reflect.DeepEqual(got, expected) {
+		t.Errorf("\n     got: %v\nexpected: %v\n", got, expected)
 	}
 }
